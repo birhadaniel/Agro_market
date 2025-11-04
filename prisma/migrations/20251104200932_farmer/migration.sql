6e1fe7ae59f314e009bb0d210773bc196fb62049
Daniel Birhanu <danielbirhanu0374@gmail.com>
2025-11-04 23:12:19 +0300

    prisma migrate build

---
+++ prisma/migrations/20251104200932_farmer/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "Farmer" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "agree" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Farmer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Farmer_email_key" ON "Farmer"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Farmer_phone_key" ON "Farmer"("phone");
